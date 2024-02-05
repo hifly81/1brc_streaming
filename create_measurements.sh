5d412907d35252dd86084a23a6eb538b3b9351e0
hifly81 <gio.marigi@gmail.com>
2024-02-05 15:18:37 +0100

    span multiple jr

--- create_measurements.sh
+++ create_measurements.sh
@@ -22,6 +22,23 @@ brew install jr
 
 echo -e "generating a measurements.csv with 1B rows in current folder. this operation will take time... [!!! BE AWARE file final size will be > 10GB !!!]"
 
-echo -e "city;temperature" > measurements.csv && jr template run -n 1_000_000_000 --seed 1 --embedded '{{city}};{{format_float "%.1f" (floating 40 5)}}' >> measurements.csv
+for i in {1..10}
+do
+  jr template run -n 100000000 --seed ${i} --embedded '{{city}};{{format_float "%.1f" (floating 40 5)}}' >> measurements${i}.csv &
+  pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+  wait $pid
+done
+
+echo -e "city;temperature" > measurements.csv
+
+for i in {1..10}
+do
+  cat measurements${i}.csv >> measurements.csv
+  rm measurements${i}.csv
+done
+
 
 echo -e "measurements.csv with 1B rows CREATED in current folder."
