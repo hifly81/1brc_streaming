19c202948da0dc80a6932c65783202e6c29b7d4a
hifly81 <gio.marigi@gmail.com>
2024-02-05 17:02:35 +0100

    added chunk to producer

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ echo -e "Wait 30 seconds to have cluster running..."
 sleep 30
 
 echo -e "Create topics [measurements, results]..."
-docker exec -it kafka1 kafka-topics --bootstrap-server kafka1:9092 --create --topic measurements --replication-factor 3 --partitions 32
-docker exec -it kafka1 kafka-topics --bootstrap-server kafka1:9092 --create --topic results --replication-factor 3 --partitions 32
+docker exec -it kafka1 kafka-topics --bootstrap-server kafka1:9092 --create --topic measurements --replication-factor 3 --partitions 32 --config message.timestamp.type=LogAppendTime
+docker exec -it kafka1 kafka-topics --bootstrap-server kafka1:9092 --create --topic results --replication-factor 3 --partitions 32 --config message.timestamp.type=LogAppendTime
 
 echo -e "Cluster ready!"
--- producer.py
+++ producer.py
@@ -1,17 +1,16 @@
 import pandas as pd
 from confluent_kafka import Producer
 
-def read_csv(file_path):
-    return pd.read_csv(file_path)
-
-def send_to_kafka(bootstrap_servers, topic, data):
-    producer = Producer({'bootstrap.servers': bootstrap_servers})
-
-    for _, row in data.iterrows():
-        print(f"Row '{row.name}' sent'")
-        producer.produce(topic, key=str(row.name), value=str(row))
-
-    producer.flush()
+def chunk(bootstrap_servers, topic, csv_file_path):
+    producer = Producer({'bootstrap.servers': bootstrap_servers, 'compression.type': 'gzip'})
+    chunksize = 1000
+    counter = 0
+    for chunk in pd.read_csv(csv_file_path, chunksize=chunksize):
+        for _, row in chunk.iterrows():
+            print(f"Chunk {counter} - Row '{row.name}' sent'")
+            producer.produce(topic, key=str(row.name), value=str(row))
+        counter = counter +1
+        producer.flush()
 
 if __name__ == "__main__":
     csv_file_path = 'measurements.csv'
@@ -19,8 +18,6 @@ if __name__ == "__main__":
     kafka_bootstrap_servers = 'kafka1:9092,kafka2:9093,kafka3:9094'
     kafka_topic = 'measurements'
 
-    csv_data = read_csv(csv_file_path)
-
-    send_to_kafka(kafka_bootstrap_servers, kafka_topic, csv_data)
+    chunk(kafka_bootstrap_servers, kafka_topic, csv_file_path)
 
     print(f"Data from CSV file '{csv_file_path}' sent to Kafka topic '{kafka_topic}'")
