30cce007d887a86509f72f9cf8c2a0f14ca01d1c
hifly81 <gio.marigi@gmail.com>
2024-02-06 13:25:05 +0100

    producer threads

--- producer.py
+++ producer.py
@@ -1,16 +1,52 @@
 import pandas as pd
+import time
+import datetime
+import concurrent.futures
 from confluent_kafka import Producer
 
-def chunk(bootstrap_servers, topic, csv_file_path):
-    producer = Producer({'bootstrap.servers': bootstrap_servers, 'compression.type': 'gzip'})
-    chunksize = 1000
-    counter = 0
-    for chunk in pd.read_csv(csv_file_path, chunksize=chunksize):
-        for _, row in chunk.iterrows():
-            print(f"Chunk {counter} - Row '{row.name} - key:{row[0]} value:{row[1]}' sent'")
-            producer.produce(topic, key=str(row[0]), value=str(row[1]))
-        counter = counter +1
-        producer.flush()
+def read_csv_chunk(filepath, chunk_size):
+    chunks = pd.read_csv(filepath, chunksize=chunk_size)
+    for chunk in chunks:
+        yield chunk
+
+def process_chunk(chunk, producer, topic):
+    sendToKafka(chunk, producer, topic)
+
+def sendMessages(bootstrap_servers, topic, csv_file_path):
+    producer = Producer({
+        'bootstrap.servers': bootstrap_servers,
+        'compression.type': 'gzip'
+    })
+    chunksize = 100000
+    ct = datetime.datetime.now()
+    start_time = time.time()
+    print(f"File sending start time '{format(ct)}'")
+    num_threads = 10
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = []
+        for chunk in read_csv_chunk(csv_file_path, chunksize):
+            futures.append(executor.submit(process_chunk, chunk, producer, topic))
+
+        # Wait for all threads to complete
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
+    producer.flush()
+    print(f"Finished sending file to kafka, processing_time='{(time.time() - start_time)}'")
+    ct = datetime.datetime.now()
+    print(f"File sending end time {format(ct)}")
+
+def sendToKafka(chunk, producer, topic):
+    for _, row in chunk.iterrows():
+        print(f"Row '{row.name}'")
+        producer.produce(topic, key=str(row[0]), value=str(row[1]), callback=acked)
+        producer.poll(0)
+    producer.flush()
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        pass
 
 if __name__ == "__main__":
     csv_file_path = 'measurements.csv'
@@ -18,6 +54,6 @@ if __name__ == "__main__":
     kafka_bootstrap_servers = 'kafka1:9092,kafka2:9093,kafka3:9094'
     kafka_topic = 'measurements'
 
-    chunk(kafka_bootstrap_servers, kafka_topic, csv_file_path)
+    sendMessages(kafka_bootstrap_servers, kafka_topic, csv_file_path)
 
     print(f"Data from CSV file '{csv_file_path}' sent to Kafka topic '{kafka_topic}'")
