494dca64b0e3dd80fbc393e12be90a9ee6b264e3
hifly81 <gio.marigi@gmail.com>
2024-02-06 13:31:13 +0100

    producer threads3

--- producer.py
+++ producer.py
@@ -10,9 +10,9 @@ def read_csv_chunk(filepath, chunk_size):
         yield chunk
 
 def process_chunk(chunk, producer, topic):
-    sendToKafka(chunk, producer, topic)
+    send_to_kafka(chunk, producer, topic)
 
-def sendMessages(bootstrap_servers, topic, csv_file_path):
+def send_messages(bootstrap_servers, topic, csv_file_path):
     producer = Producer({
         'bootstrap.servers': bootstrap_servers,
         'compression.type': 'gzip'
@@ -36,13 +36,13 @@ def sendMessages(bootstrap_servers, topic, csv_file_path):
     ct = datetime.datetime.now()
     print(f"File sending end time {format(ct)}")
 
-def sendToKafka(chunk, producer, topic):
+def send_to_kafka(chunk, producer, topic):
     for _, row in chunk.iterrows():
         print(f"Row '{row.name}'")
         producer.produce(topic, key=str(row[0]), value=str(row[1]), callback=acked)
         producer.poll(0)
     producer.flush()
-    
+
 def acked(err, msg):
     if err is not None:
         print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
@@ -55,6 +55,6 @@ if __name__ == "__main__":
     kafka_bootstrap_servers = 'kafka1:9092,kafka2:9093,kafka3:9094'
     kafka_topic = 'measurements'
 
-    sendMessages(kafka_bootstrap_servers, kafka_topic, csv_file_path)
+    send_messages(kafka_bootstrap_servers, kafka_topic, csv_file_path)
 
     print(f"Data from CSV file '{csv_file_path}' sent to Kafka topic '{kafka_topic}'")
