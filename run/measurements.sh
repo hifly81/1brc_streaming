2194eed814992f2cb6822490fbc51ca92fd95e4b
ram-pi <pierpaolo.rametta@gmail.com>
2024-02-07 11:39:27 +0100

    ADD: support for x86_64 arch

--- run/measurements.sh
+++ run/measurements.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-chmod +x measurements
-./measurements
+chmod +x measurements_x86_64
+chmod +x measurements_arm
+
+# get the architecture of the system
+arch=$(uname -m)
+
+# if architecture is x86_64 then run measurements_x86_64 else run measurements_arm
+if [ "$arch" == "x86_64" ]; then
+    ./measurements_x86_64
+else
+    ./measurements_arm
+fi
