c81df7733dfbf1f9b06b0210f6090fa2cbe863c5
ram-pi <pierpaolo.rametta@gmail.com>
2024-02-14 18:47:10 +0100

    ADD: shebang producer

--- run/producer.sh
+++ run/producer.sh
@@ -1,1 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+KAFKA_VERSION=3.6.1
+KAFKA_DIR=kafka_2.13-$KAFKA_VERSION
+
+# move to the directory of the script
+cd producer || exit
+
+# download kafka if directory does not exist
+if [ ! -d $KAFKA_DIR ]; then
+    wget https://downloads.apache.org/kafka/$KAFKA_VERSION/kafka_2.13-$KAFKA_VERSION.tgz
+    tar -xzf kafka_2.13-$KAFKA_VERSION.tgz
+    rm kafka_2.13-$KAFKA_VERSION.tgz
+fi
+
+# Split the measurements.csv file in 10 smaller files
+rm -rf files
+mkdir -p files
+mv ../data.csv files
+cd files || exit
+#split -d -n 1 data.csv splitted
+split -l 10000000 -d data.csv splitted
+mv data.csv ../..
+cd ..
+
+# run java program
+./app &>app.log
+
+rm -fr $KAFKA_DIR
+rm -rf files
+pkill -9 java
